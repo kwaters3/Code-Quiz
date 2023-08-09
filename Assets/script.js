a93ce49e99adac04939d9cff4000b3e180b13d33
kwaters3 <knickler3@gmail.com>
2023-08-09 10:44:47 -0400

    updated javascript to have timer reduce by 10 seconds for incorrect answers and added the submit button

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,7 @@
 const startButton = document.getElementById("start-button");
 const introSection = document.querySelector(".intro");
 const quizSection = document.querySelector(".quiz");
-const questionElement = document.getElementById ("question");
+const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 const timeRemainingElement = document.getElementById("time-remaining");
@@ -19,78 +19,86 @@ startButton.addEventListener("click", startQuiz);
 function startQuiz() {
   introSection.style.display = "none";
   quizSection.style.display = "block";
-  countDown(); // Start the countdown timer
+  countDown();
   showQuestion();
 }
 
 // This is the Question button
-function showQuestion () {
-  resetState ();
+function showQuestion() {
+  resetState();
   nextButton.innerHTML = "Next Question"
   let currentQuestion = questions[currentQuestionIndex];
   let questionNum = currentQuestionIndex + 1;
   questionElement.innerHTML = questionNum + ". " + currentQuestion.question;
 
-  currentQuestion.answers.forEach(answer => {
+  currentQuestion.answers.forEach(function (answer) {
     const button = document.createElement("button");
     button.innerHTML = answer.text;
     button.classList.add("btn");
     answerButtons.appendChild(button);
-    if(answer.correct){
+    if (answer.correct) {
       button.dataset.correct = answer.correct;
     }
     button.addEventListener("click", selectAnswer);
   });
 }
 
+
 function countDown() {
   const timer = setInterval(function () {
-    timeRemaining--;
-    timeRemainingElement.textContent = timeRemaining;
-    if (timeRemaining <= 0 || currentQuestionIndex >= questions.length) {
-      clearInterval(timer);
-      gameOver();
+    if (timeRemaining > 0 && currentQuestionIndex < questions.length) {
+      timeRemaining--;
+      timeRemainingElement.textContent = timeRemaining;
     }
-  }, 500); 
+
+    if (timeRemaining <= 0 || currentQuestionIndex >= questions.length) {
+      showScore();
+      clearInterval(timer);
+      timeRemaining = 0; // Set the timer to 0 when the quiz is over
+      timeRemainingElement.textContent = timeRemaining;
+    }
+  }, 1000);
 }
 
 
-function gameOver() {
-  resetState();
-  questionElement.innerHTML = `Game Over! Your final score is ${score} out of ${questions.length}!`;
-  showScore();
+
+function pressNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
 }
 
 
-function showScore() {
-  resetState();
-  questionElement.innerHTML = `Your score is ${score} out of ${questions.length}!<br>Enter your initials: <input type="text" id="initials-input">`;
-  nextButton.innerHTML = "Save Score";
-  nextButton.addEventListener("click", saveScore);
-  nextButton.style.display = "block";
-  saveScore();
-}
+nextButton.addEventListener("click", () => {
+  pressNextButton();
+});
 
 
 // This removes the Answer 1,2,3,4 choices from the HTML
-function resetState (){
+function resetState() {
   nextButton.style.display = "none";
-  while(answerButtons.firstChild){answerButtons.removeChild(answerButtons.firstChild);
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
   }
 }
 
-function selectAnswer(event){
+
+function selectAnswer(event) {
   const selectedBtn = event.target;
   const isCorrect = selectedBtn.dataset.correct === "true";
-  if (isCorrect){
+  if (isCorrect) {
     selectedBtn.classList.add("correct");
-    score++; 
-  }else {
+    score++;
+  } else {
     selectedBtn.classList.add("wrong");
+    timeRemaining -= 10;
   }
 
-  Array.from(answerButtons.children).forEach(button => {
-    if(button.dataset.correct === "true"){
+  Array.from(answerButtons.children).forEach(function (button) {
+    if (button.dataset.correct === "true") {
       button.classList.add("correct");
     }
     button.disabled = true;
@@ -99,18 +107,23 @@ function selectAnswer(event){
 }
 
 
-function pressNextButton() {
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questions.length) {
-      showQuestion();
-    } else {
-      gameOver();
-    }
-  }
 
-nextButton.addEventListener("click", () => {
-  pressNextButton(); 
-});
+function showScore() {
+  resetState();
+  questionElement.innerHTML = `Your score is ${score} out of ${questions.length}!<br>Enter your initials to save your score: <input type="text" id="initials"><br>
+  <button type="button" id="initialbtn">submit</button>`;
+
+  
+  const initialSubmitButton = document.getElementById("initialbtn");
+  initialSubmitButton.addEventListener("click", saveScore);
+}
+
+function saveScore() {
+  const initialsInput = document.getElementById("initials");
+  const initials = initialsInput.value;
+  questionElement.innerHTML = "Score saved! Thank you.";
+  initialsInput.value = "";
+}
 
 
 
@@ -120,80 +133,80 @@ const questions = [
   {
     question: "What is JavaScript?",
     answers: [
-      {text: "The World Wide Web", correct: false},
-      {text: "The Matrix", correct: false},
-      {text: "A Programming Language", correct: true},
-      {text: "A Functional Language", correct: false},
+      { text: "The World Wide Web", correct: false },
+      { text: "The Matrix", correct: false },
+      { text: "A Programming Language", correct: true },
+      { text: "A Functional Language", correct: false },
     ]
   },
 
   {
     question: "Which of these is not a variable in JavaScript?",
     answers: [
-      {text: "var", correct: false},
-      {text: "let", correct: false},
-      {text: "const", correct: false},
-      {text: "function", correct: true},
+      { text: "var", correct: false },
+      { text: "let", correct: false },
+      { text: "const", correct: false },
+      { text: "function", correct: true },
     ]
   },
 
   {
     question: "JavaScript has several data types, which of the following is not a JavaScript data type?",
     answers: [
-      {text: "Number", correct: false},
-      {text: "Array", correct: true},
-      {text: "Boolean", correct: false},
-      {text: "String", correct: false},
+      { text: "Number", correct: false },
+      { text: "Array", correct: true },
+      { text: "Boolean", correct: false },
+      { text: "String", correct: false },
     ]
   },
 
   {
     question: "How do you check the type of variable?",
     answers: [
-      {text: "document.type", correct: false},
-      {text: "console.log", correct: false},
-      {text: "typeof", correct: true},
-      {text: "windows.type", correct: false},
+      { text: "document.type", correct: false },
+      { text: "console.log", correct: false },
+      { text: "typeof", correct: true },
+      { text: "windows.type", correct: false },
     ]
   },
 
   {
     question: "What does the === operator do in JavaScript",
     answers: [
-      {text: "Assigns a value to a variable", correct: false},
-      {text: "Compares values for loose equality", correct: false},
-      {text: "Creates a new variable", correct: false},
-      {text: "Compares values for strict equality", correct: true},
+      { text: "Assigns a value to a variable", correct: false },
+      { text: "Compares values for loose equality", correct: false },
+      { text: "Creates a new variable", correct: false },
+      { text: "Compares values for strict equality", correct: true },
     ]
   },
 
   {
     question: "What is the output of the following code: console.log(typeof null)?",
     answers: [
-      {text: "object", correct: false},
-      {text: "null", correct: true},
-      {text: "undefined", correct: false},
-      {text: "string", correct: false},
+      { text: "object", correct: false },
+      { text: "null", correct: true },
+      { text: "undefined", correct: false },
+      { text: "string", correct: false },
     ]
   },
 
   {
     question: "What does the NaN value represent in JavaScript?",
     answers: [
-      {text: "Not a Number", correct: true},
-      {text: "Null value", correct: false},
-      {text: "Negative number", correct: false},
-      {text: "New Array Now", correct: false},
+      { text: "Not a Number", correct: true },
+      { text: "Null value", correct: false },
+      { text: "Negative number", correct: false },
+      { text: "New Array Now", correct: false },
     ]
   },
 
   {
     question: "What is the purpose of the document.getElementById() function in JavaScript?",
     answers: [
-      {text: " It adds a new HTML element to the document", correct: false},
-      {text: "It retrieves an HTML element from the document by its ID", correct: true},
-      {text: "It changes the font style of an element", correct: false},
-      {text: "It removes an HTML element from the document", correct: false},
+      { text: " It adds a new HTML element to the document", correct: false },
+      { text: "It retrieves an HTML element from the document by its ID", correct: true },
+      { text: "It changes the font style of an element", correct: false },
+      { text: "It removes an HTML element from the document", correct: false },
     ]
   }
 ];
